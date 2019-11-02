aea75f739d1c3b0f9b6985ce111231395a7bdbf8
caueguedes <caueguedes@univem.edu.br>
2019-11-02 18:30:48 -0300

    working finally

--- Scripts/extractFrames.py
+++ Scripts/extractFrames.py
@@ -2,7 +2,46 @@ import sys
 import argparse
 import cv2
 import os
+import numpy as np
 
+def extractImages(pathIn, frameRate=100):
+    time = 0
+    vidcap = cv2.VideoCapture(pathIn)
+    time += frameRate
+    vidcap.set(cv2.cv.CV_CAP_PROP_POS_MSEC, time)
+    success,image = vidcap.read()
+    print ('Read a new frame: ', success)
+    cv2.imwrite( pathIn[:-4] + r"-frame%.0f.png" % (time/frameRate), image)
+    print ( pathIn[:-4] + "Image written  frame%.0f.png" % (time/frameRate))
+
+    previous_image = image
+    while success:
+        time += frameRate
+        vidcap.set(cv2.cv.CV_CAP_PROP_POS_MSEC, time) 
+        success,image = vidcap.read()
+        print ('Read a new frame: ', success)
+        if not np.array_equiv(previous_image, image):
+            cv2.imwrite( pathIn[:-4] + r"-frame%.0f.png" % (time/frameRate), image)
+            print ( pathIn[:-4] + "Image written  frame%.0f.png" % (time/frameRate))
+            previous_image = image
+        else:
+            success = False
+            
+if __name__=="__main__":
+    argument = argparse.ArgumentParser()
+    argument.add_argument( 
+            "--pathIn", 
+            help="path to video",
+            )
+    argument.add_argument(
+            "--frameRate", 
+            help="video capture frameRate in miliseconds",
+            type=float
+            )
+    args = argument.parse_args()
+    print(args)
+
+    extractImages(args.pathIn, args.frameRate
 
 #def extractImages(pathIn, frameRate=100):
 ##def extractImages(pathIn, pathOut, frameRate=100):
@@ -39,47 +78,47 @@ import os
 #    extractImages(args.pathIn, args.frameRate)  
 
 
-def extractImages(pathIn, pathOut, frameRate=100):
-    time = 0
-    vidcap = cv2.VideoCapture(pathIn)
-    success = True
-    while success:
-        time += frameRate
-        vidcap.set(cv2.cv.CV_CAP_PROP_POS_MSEC, time) 
-        success,image = vidcap.read()
-        print ('Read a new frame: ', success)
-        if success:
-            cv2.imwrite( pathOut + r"frames/frame%.0f.jpg" % (time/frameRate), image)
-            print ("Image written  frame%.0f.jpg" % (time/frameRate))
-            #success, image = vidcap.read()
+#def extractImages(pathIn, pathOut, frameRate=100):
+#    time = 0
+#    vidcap = cv2.VideoCapture(pathIn)
+#    success = True
+#    while success:
+#        time += frameRate
+#        vidcap.set(cv2.cv.CV_CAP_PROP_POS_MSEC, time) 
+#        success,image = vidcap.read()
+#        print ('Read a new frame: ', success)
+#        if success:
+#            cv2.imwrite( pathOut + r"frames/frame%.0f.jpg" % (time/frameRate), image)
+#            print ("Image written  frame%.0f.jpg" % (time/frameRate))
+#           #success, image = vidcap.read()
 
-if __name__=="__main__":
-    argument = argparse.ArgumentParser()
-    argument.add_argument( 
-            "--pathIn", 
-            help="path to video",
-            )
-    argument.add_argument(
-            "--pathOut", 
-            help="path to images",
-            )
-    argument.add_argument(
-            "--frameRate", 
-            help="video capture frameRate in miliseconds",
-            type=float
-            )
-    args = argument.parse_args()
-    print(args)
+#if __name__=="__main__":
+#    argument = argparse.ArgumentParser()
+#    argument.add_argument( 
+#            "--pathIn", 
+#            help="path to video",
+#            )
+#    argument.add_argument(
+#            "--pathOut", 
+#            help="path to images",
+#            )
+#    argument.add_argument(
+#            "--frameRate", 
+#            help="video capture frameRate in miliseconds",
+#            type=float
+#            )
+#    args = argument.parse_args()
+#    print(args)#
 
-    pathOutput = args.pathOut + 'frames'
+#    pathOutput = args.pathOut + 'frames'
 	
-    if os.path.exists(pathOutput):
-        if os.listdir(pathOutput) > 0:
-             for file in os.listdir(pathOutput):
-                 os.remove(pathOutput + '/' + file)
-             print('Removed all files from ' + pathOutput)
-    else: 
-        os.mkdir(pathOutput)
-        print('Dir ' + pathOutput)
+#    if os.path.exists(pathOutput):
+#        if os.listdir(pathOutput) > 0:
+#             for file in os.listdir(pathOutput):
+#                 os.remove(pathOutput + '/' + file)
+#             print('Removed all files from ' + pathOutput)
+#    else: 
+#        os.mkdir(pathOutput)
+#        print('Dir ' + pathOutput)#
 
-    extractImages(args.pathIn, args.pathOut, args.frameRate)  
+#    extractImages(args.pathIn, args.pathOut, args.frameRate)  
